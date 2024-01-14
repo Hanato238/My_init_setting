f8e04e4d991724039bd7746712f8e10a5b221b33
Hanato <les.ensoleilles@gmail.com>
2024-01-14 11:14:21 +0900

    ubuntu bash

---
+++ ubuntu/ubuntu.sh
@@ -0,0 +1,11 @@
+# google chromeをインストールする
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
+
+# VScodeをインストールする
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code
