3a1528c319d202cbbfbddefa61965524f2ec2916
Hanato <les.ensoleilles@gmail.com>
2024-01-16 15:12:34 +0900

    add chrome remote desktop

--- ubuntu/ubuntu.sh
+++ ubuntu/ubuntu.sh
@@ -3,6 +3,12 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
+# chrome remote desktopをインストールする
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg -i chrome-remote-desktop_current_amd64.deb
+sudo apt intall --assume0yes --fix-broken
+rm chrome-remote-desktop_current_amd64.deb
+
 # google chromeをインストールする
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
