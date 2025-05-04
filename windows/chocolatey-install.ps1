9d1b2b414375813e9c55946f5ab0d90677f72c16
hanato238 <les.ensoleilles@gmail.com>
2025-05-04 15:57:44 +0900

    modify

--- windows/chocolatey-install.ps1
+++ windows/chocolatey-install.ps1
@@ -1,18 +1,24 @@
 # Run PowerShell as Administrator before executing this script
 
 # Set execution policy to allow script execution
-Set-ExecutionPolicy Bypass -Scope Process -Force; 
+##? Set-ExecutionPolicy Bypass -Scope Process -Force; 
 
 # Install Chocolatey
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Chocolateyがインストールされているか確認
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    return
+}
+
 
 # Verify installation
 choco --version
 
 # Google Chromeをインストール
-choco install googlechrome -y
+choco install googlechrome --ignore-checksums -y
 # Google Driveをインストール
-choco install google-drive-file-stream -y
+choco install googledrie --ignore-checksums -y
+choco install google-drive-file-stream --ignore-checksums -y
 # Gitをインストール
 choco install git -y
 
