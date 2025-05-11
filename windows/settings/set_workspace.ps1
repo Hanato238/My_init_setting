7af791a9f9346d6986b2217b54b8fef1c01c403b
hanato238 <les.ensoleilles@gmail.com>
2025-05-11 14:08:28 +0900

    a

--- windows/settings/set_workspace.ps1
+++ windows/settings/set_workspace.ps1
@@ -4,6 +4,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 $workspacePath = "C:\Users\lesen\workspace"
 if (-Not (Test-Path -Path $workspacePath)) {
     New-Item -Path $workspacePath -ItemType Directory -Force
+    Write-Host "Workspace directory created at $workspacePath" -ForegroundColor Green
 }
 
 # Set the workspace path in PowerShell profile
@@ -13,7 +14,12 @@ $profilePath = $PROFILE
 if (Test-Path $profilePath) {
     if (-not (Get-Content $profilePath | Select-String -Pattern ([regex]::Escape($lineToAdd)))) {
         Add-Content -Path $profilePath -Value "`n$lineToAdd"
+        Write-Host "Workspace path added to PowerShell profile." -ForegroundColor Green
+    }
+    else{
+        Write-Host "Workspace path already exists in PowerShell profile." -ForegroundColor Yellow
     }
 } else {
     Set-Content -Path $profilePath -Value $lineToAdd
+    Write-Host "PowerShell profile created and workspace path added." -ForegroundColor Green
 }
