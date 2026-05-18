22f13dd6468b317ef76e0e6c6916ca7ec190539b
hanato238 <les.ensoleilles@gmail.com>
2026-05-18 19:26:09 +0900

    a

--- windows/installer/Install-Apps.ps1
+++ windows/installer/Install-Apps.ps1
@@ -8,10 +8,13 @@ if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
 
 # Install all packages in one command
 Write-Host "Installing apps via Chocolatey..." -ForegroundColor Cyan
-choco install googlechrome googledrive google-drive-file-stream gcloudsdk python39 python310 python311 python312 python313 python314 vim curl gsudo mingw winget tree procmon wireshark powertoys expressvpn uv jq git nodejs-lts vscode materialicon-vscode ngrok wsl-ubuntu-2204 docker-desktop windows-sdk-10.1 awscli bitwarden bitwarden-chrome bitwarden-cli spacedesk-server choco-cleaner --ignore-checksums -y
+choco install googlechrome googledrive google-drive-file-stream gcloudsdk python39 python310 python311 python312 python313 python314 vim curl gsudo mingw winget tree procmon wireshark powertoys expressvpn uv jq git nodejs-lts vscode materialicon-vscode ngrok docker-desktop windows-sdk-10.1 awscli bitwarden bitwarden-chrome bitwarden-cli spacedesk-server choco-cleaner --ignore-checksums -y
 
 # upgrade all packages
 choco upgrade all -y
+
+# install Ubuntu
+wsl --install -d Ubuntu26.04
 
 # Setup PowerShell Secret Management
 Write-Host "Installing PowerShell modules..." -ForegroundColor Cyan
